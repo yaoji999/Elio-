d2d0000fa036bbcca37eb8fd788ae52725a7693a
yaoji999 <marcel.ch@hotmail.com>
2025-05-05 22:30:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def results():
         jours = int(request.form['jours'])
         regime = request.form['regime']
 
-        with open('ELIO_Plan_90_Jours.json', 'r', encoding='utf-8') as f:
+        with open('ELIO_Calories_90_Jours.json', 'r', encoding=' as f:
             data = json.load(f)
 
         plans = data[:jours]

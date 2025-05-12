187daae17ba0c99261c863e49df0bb3062de66da
yaoji999 <marcel.ch@hotmail.com>
2025-05-12 20:28:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def results():
 @app.route('/download', methods=['POST'])
 def download():
     if 'week' not in session:
-        return redirect(url_for('formulaire'))
+        return redirect(url_for('index'))
 
     start = session['week'] * 7
     end = min(start + 7, session['jours'])
